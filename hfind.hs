47d89ce2593c44974b343bfe950b2fdc03e00a2c
Julian Macagno <julian.macagno55@gmail.com>
2019-01-22 19:50:36 -0300

    Now hfind accepts cmd comands
    Added the options to accepts commands that requires filepaths at end like "rm"

--- hfind.hs
+++ hfind.hs
@@ -4,13 +4,13 @@ module Main where
 import Options.Applicative
 import Data.Semigroup ((<>))
 import System.FilePath.Find
+import System.Process
 --import System.FilePath.Glob
 --import System.FilePath.Manip
 import qualified Data.List as DL
 
-
-data Search = FileName { fname :: String, path :: String } 
-            | FileType { ftype :: String, path :: String }
+data Search = FileName { fname :: String, path :: String, exec :: String } 
+            | FileType { ftype :: String, path :: String, exec :: String }
 
 searchByFilename :: Parser Search
 searchByFilename = FileName
@@ -24,6 +24,12 @@ searchByFilename = FileName
        <> short 'p'
        <> metavar "path"
        <> help "path where to search for" )
+      <*> option str
+        ( long "exec"
+       <> short 'e'
+       <> value ""
+       <> metavar "exec"
+       <> help "command to be excecuted at search result")
 
 searchByFiletype :: Parser Search
 searchByFiletype = FileType
@@ -37,29 +43,37 @@ searchByFiletype = FileType
         <> short 'p'
         <> metavar "path"
         <> help "path where to search for" )
+       <*> option str
+         ( long "exec"
+        <> short 'e'
+        <> value ""
+        <> metavar "exec"
+        <> help "command to be excecuted at search result")
 
-hablar :: Parser Search
-hablar = searchByFiletype <|> searchByFilename
+searcher :: Parser Search
+searcher = searchByFiletype <|> searchByFilename
 
 main :: IO ()
 main = search =<< execParser opts
 
 opts :: ParserInfo Search
-opts = info (hablar <**> helper)
+opts = info (searcher <**> helper)
   ( fullDesc
   <> progDesc "Search files on the given path and filter them by filename or filetype"
   <> header "fname - a test for optparse-applicative" )
 
 search :: Search -> IO ()
-search (FileName filename path) = do 
-    putStrLn $ "Buscar por nombre de archivo" ++ filename ++ " en " ++ path
+search (FileName filename path command) = do 
+    putStrLn $ "Buscar por nombre de archivo " ++ filename ++ " en " ++ path
     files <- find always (fileType ==? RegularFile) path
-    mapM_ print (filter (DL.isInfixOf filename) files)
-search (FileType filetype path) = do
+    let filteredFiles = filter (DL.isInfixOf filename) files
+    if command == ""
+    then mapM_ print filteredFiles
+    else mapM_ callCommand (map (command++) filteredFiles)
+search (FileType filetype path command) = do
     putStrLn $ "Buscar por tipo de archivo " ++ filetype ++ " en " ++ path
     files <- find always (fileType ==? RegularFile &&? extension ==? filetype) path
-    mapM_ print files
+    if command == ""
+    then mapM_ print files
+    else mapM_ callCommand (map (command++) files)
 --search _ = return ()
-
--- files <- find always (fileType ==? RegularFile &&? extension ==? ".hs") fstParam
--- print (filter (DL.isInfixOf sndParam) files)
